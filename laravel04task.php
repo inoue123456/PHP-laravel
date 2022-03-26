cf94e3329c41d282cffa64915f37cbbd1926186e
inoue123456 <yuta1511814@gmail.com>
2022-03-26 04:20:54 +0000

    laravel04task2

--- laravel04task.php
+++ laravel04task.php
@@ -4,7 +4,7 @@ function sum($num) {
     return $num * 2;
 }
 echo sum(5);
-echo "\n";
+
 //2.$a と $b を仮引数に持ち、　$a と $b　を足した結果を返す関数を作成
 function add($a, $b){
     return $a + $b;
@@ -23,15 +23,15 @@ echo mul(array(1, 3, 5, 7, 9));
 
 //4.【応用】　下記のプログラムは、配列の中で1番大きい値を返す max_array という関数を実装しようとしています。途中の部分を完成させてください
 function max_array($arr){
-// とりあえず配列の最初の要素を一番大きい値とする
- $max_number = $arr[0];
- foreach($arr as $a){
- //どうしたらいいかわからない・・・・
- if($max_number < $a){
- $max_number = $a;
- }
- }
- return $max_number;
+    // とりあえず配列の最初の要素を一番大きい値とする
+    $max_number = $arr[0];
+    foreach($arr as $a){
+         //どうしたらいいかわからない・・・・
+         if($max_number < $a){
+             $max_number = $a;
+         }
+    }
+    return $max_number;
  }
  echo max_array(array(10, 15, 8));
  
