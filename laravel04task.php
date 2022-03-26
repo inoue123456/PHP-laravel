044e493ac6d8fa662a3299ea8ac8f79c7ebe1cc0
inoue123456 <yuta1511814@gmail.com>
2022-03-26 04:09:29 +0000

    laravel04task

--- laravel04task.php
+++ laravel04task.php
@@ -1,25 +1,23 @@
 <?php
 //1.引数に数値を指定して実行すると、数値を2倍にして返す関数を作成
 function sum($num) {
-    $result1 = $num * 2;
-    return $result1;
+    return $num * 2;
 }
 echo sum(5);
-
+echo "\n";
 //2.$a と $b を仮引数に持ち、　$a と $b　を足した結果を返す関数を作成
 function add($a, $b){
-    $result2 = $a + $b;
-    return $result2;
+    return $a + $b;
 }
 echo add(2, 3);
 
 //3.$arr という配列の仮引数を持ち、数値が入った配列array(1, 3, 5 ,7, 9) を渡すとその要素をすべてかけた結果を返す関数を作成
 function mul($arr){
-    $result3 = $arr[0];
+    $result = $arr[0];
     for($i = 1; $i < count($arr); $i++){
-        $result3 *= $arr[$i];
+        $result *= $arr[$i];
     }
-        return $result3;
+        return $result;
 }
 echo mul(array(1, 3, 5, 7, 9));
 
